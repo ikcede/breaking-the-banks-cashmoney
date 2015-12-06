3b924aba0b483df0cd66e453844774fa9161e654
Charlie <csh342@nyu.edu>
2015-12-06 03:38:48 +0000

    dash nav

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,48 @@ router.get('/', function(req, res, next) {
     res.render('landing', { title: 'Credit 22', layout: false });
 });
 
-router.get('/dashboard', function(req, res, next) {
-    res.render('dashboard', { title: 'Dashboard', layout: 'layouts/dashlayout' });
-});
-
 router.get('/signup', function(req, res, next) {
     res.render('signup', { title: 'Sign Up' });
 });
 
+router.get('/dashboard', function(req, res, next) {
+    res.render('dashboard', {
+        title: 'Dashboard', 
+        layout: 'layouts/dashlayout', 
+        active: {
+            home: true, loan: false, pay: false, settings: false   
+        }
+    });
+});
+
+router.get('/loan', function(req, res, next) {
+    res.render('loan', { 
+        title: 'New Loan', 
+        layout: 'layouts/dashlayout', 
+        active: {
+            home: false, loan: true, pay: false, settings: false   
+        }
+    });
+});
+
+router.get('/pay', function(req, res, next) {
+    res.render('pay', { 
+        title: 'Pay Loans', 
+        layout: 'layouts/dashlayout', 
+        active: {
+            home: false, loan: false, pay: true, settings: false   
+        }
+    });
+});
+
+router.get('/settings', function(req, res, next) {
+    res.render('settings', { 
+        title: 'Settings', 
+        layout: 'layouts/dashlayout', 
+        active: {
+            home: false, loan: false, pay: false, settings: true   
+        }
+    });
+});
 
 module.exports = router;
